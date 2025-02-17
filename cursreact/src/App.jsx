964962d3a81d775b083df1e460c083726e6097b2
Oriol Tinoco <otinoco@digiocio.com>
2025-02-17 20:23:19 +0100

    Codi 5 - Events sense parametres

--- cursreact/src/App.jsx
+++ cursreact/src/App.jsx
@@ -1,28 +1,18 @@
 /* eslint-disable no-constant-condition */
 import './App.css'
 
-function sumar(){
-  return 2 + 2
-}
-
-const nom = "Joan"
-
-const inlineStyle = {
-  color: "#2ecc71",
-  fontSize: "26px"
-}
 function App() {
+  const functionClick = () => {
+    console.log('onClick')
+  }
+
+  const functionOnMouseOver = () => {
+    console.log('onMouseOver')
+  }
     return (
     <>
-      <p style={inlineStyle}>Curs React</p>
-      <div className='red'>
-        <h2>Children</h2>
-      </div>
-      <h1 className='red'>Funcions { sumar() }</h1>
-      <h1>Operacions Matematiques { 3 + 2 }</h1>
-      <h1>String { `Hola ${nom}` }</h1>
-      <h1>Logica / Ternari { (3 + 2) > 6 ? "Mayor" : "Menor" }</h1>
-      <img src="" alt="" />
+      <button onClick={functionClick}>On Click</button>
+      <h1 onMouseOver={functionOnMouseOver}>Mous Over Event</h1>
     </>
   )
 }
