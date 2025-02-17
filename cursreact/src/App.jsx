5d2982c988743e73297fc2d6c0da3008a33e18ea
Oriol Tinoco <otinoco@digiocio.com>
2025-02-17 20:10:13 +0100

    Codi 3 - Interpolació de JS

--- cursreact/src/App.jsx
+++ cursreact/src/App.jsx
@@ -1,10 +1,21 @@
+/* eslint-disable no-constant-condition */
 import './App.css'
 
+function sumar(){
+  return 2 + 2
+}
+
+const nom = "Joan"
 function App() {
     return (
     <>
-      <h1 className='color'>Hola k ase?</h1>
-      <h1>Quieres aprender React?</h1>
+      <div className='red'>
+        <h2>Children</h2>
+      </div>
+      <h1 className='red'>Funcions { sumar() }</h1>
+      <h1>Operacions Matematiques { 3 + 2 }</h1>
+      <h1>String { `Hola ${nom}` }</h1>
+      <h1>Logica / Ternari { (3 + 2) > 6 ? "Mayor" : "Menor" }</h1>
       <img src="" alt="" />
     </>
   )
