c899e4073c21be68bce52970249269888d05fb28
Oriol Tinoco <otinoco@digiocio.com>
2025-02-18 11:50:07 +0100

    Codi 10 - Estils globals i locals

--- cursreact/src/components/Header.jsx
+++ cursreact/src/components/Header.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
+import './Header.css';
 import logo from '../img/qx7jYSYm_400x400.jpg';
 import Salutacio from './Salutacio.jsx';
 class Header extends Component {
     render() {
         var usuari = "Oriol";
         return(
-            <h1><img src={logo} height="50px" /> El teu portal de montanya. <Salutacio nick={usuari} /> </h1>
+            <h1 className='salutacio'><img src={logo} height="50px" /> El teu portal de montanya. <Salutacio nick={usuari} /> </h1>
         )
     }
 }
