553dcbb7e9945cd2258cbb6dce24ae1bcc78e1fc
Oriol Tinoco <otinoco@digiocio.com>
2025-02-18 08:59:52 +0100

    Codi 8 - Components funcionals en React

--- cursreact/src/Header.jsx
+++ cursreact/src/Header.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import logo from './qx7jYSYm_400x400.jpg';
-
+import Salutacio from './Salutacio.js';
 class Header extends Component {
     render() {
+        var usuari = "Oriol";
         return(
-            <h1><img src={logo} height="50px" /> El teu portal de montanya.</h1>
+            <h1><img src={logo} height="50px" /> El teu portal de montanya. <Salutacio nick={usuari} /> </h1>
         )
-    }    
+    }
 }
-export default Header
+ export default Header;
---
+++ cursreact/src/Salutacio.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from 'react'; 
+function Menu({nick}){ 
+    return (
+        <>
+            Benvingut/da {nick}
+        </> 
+    ) 
+} 
+export default Menu;
