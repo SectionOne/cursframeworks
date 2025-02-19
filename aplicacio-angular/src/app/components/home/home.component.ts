4c0737e7999f63c95dbfa40d9c32862af421a323
Oriol Tinoco <otinoco@digiocio.com>
2025-02-19 22:19:10 +0100

    Codi 37 - Creant servei a Angular

--- aplicacio-angular/src/app/components/home/home.component.ts
+++ aplicacio-angular/src/app/components/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HttpClientModule, CommonModule], // Importa HttpClientModule aquí
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  providers: [ApiService], // Elimina aquesta línia (no és necessària)
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  posts: any[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getPosts().subscribe((data: any[]) => {
+      this.posts = data;
+    });
+  }
 }
