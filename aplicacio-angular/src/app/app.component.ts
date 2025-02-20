3b3706f6c97cd4540c26b52478e12c55fbde4bcf
Oriol Tinoco <otinoco@digiocio.com>
2025-02-20 23:01:01 +0100

    Codi 45 - Treballem amb data binding

--- aplicacio-angular/src/app/app.component.ts
+++ aplicacio-angular/src/app/app.component.ts
@@ -9,6 +9,14 @@ import { ComponentB } from './components/component-b/component-b.component'; // 
 import { PareComponentViewChild } from './components/viewchild/pare/pare.component'; // Importa el component pare
 import { NgifComponent } from './components/ngif/ngif.component';
 import { NgStyleComponent } from './components/ngstyle/ngstyle.component';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
+
+// Definim una interfície per al model de dades
+interface Usuari {
+  nom: string;
+  edat: number;
+  actiu: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -23,10 +31,26 @@ import { NgStyleComponent } from './components/ngstyle/ngstyle.component';
     PareComponentViewChild,
     NgifComponent,
     NgStyleComponent,
+    FormsModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  // Definim propietats amb tipus
   title = 'aplicacio-angular';
+  titol: string = 'Data Binding en Angular';
+  usuari: Usuari = { nom: 'Pere', edat: 30, actiu: true };
+  missatge: string = '';
+
+  // Mètode per gestionar un esdeveniment
+  actualitzarNom(nouNom: string): void {
+    this.usuari.nom = nouNom;
+    this.missatge = `Nom actualitzat a: ${nouNom}`;
+  }
+
+  // Mètode per alternar l'estat actiu
+  alternarActiu(): void {
+    this.usuari.actiu = !this.usuari.actiu;
+  }
 }
--- aplicacio-angular/src/app/app.module.ts
+++ aplicacio-angular/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UserModule } from './modules/user/user.module'; // Import del mòdul
+import { FormsModule } from '@angular/forms'; // Importem FormsModule
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UserModule], // Afegim el nou mòdul
+  imports: [BrowserModule, UserModule, FormsModule], // Afegim el nou mòdul
   providers: [],
   bootstrap: [AppComponent],
 })
