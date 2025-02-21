e7a8ef67f5929cb948b7185599b92c004a0049d2
Oriol Tinoco <otinoco@digiocio.com>
2025-02-21 11:07:38 +0100

    Codi 47 - Creant un component amb Vue

---
+++ vue-project/src/components/ElMeuComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="mi-componente">
+      <h1>{{ titol }}</h1>
+      <p>{{ missatge }}</p>
+      <button @click="canviarMissatge">Canvia el missatge</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MiComponente', // Nom del component
+    data() {
+      return {
+        titol: 'Benvingut al meu component!',
+        missatge: 'Aquest és un component bàsic de Vue.'
+      };
+    },
+    methods: {
+      canviarMissatge() {
+        this.missatge = 'Has canviat el missatge!';
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .mi-componente {
+    border: 1px solid #ccc;
+    padding: 20px;
+    margin: 20px;
+    text-align: center;
+  }
+  
+  h1 {
+    color: #42b983;
+  }
+  </style>
